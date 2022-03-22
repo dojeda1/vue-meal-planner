c3446b639438d0cb9c32b7d6f8f5bba8db97a4a7
Dominic Ojeda <dominicojeda.coding@gmail.com>
2022-03-22 15:14:49 -0600

    mobile nav created.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,21 +2,29 @@
     <!-- NavBar -->
     <nav class="nav-wrapper light-green darken-1">
         <div class="container">
-            <a href="./index.html" class="brand-logo"><img :src="logo" id="logo"
+            <a href="/" class="brand-logo"><img :src="logo" id="logo"
                     class="left valign-wrapper"><span class="hide-on-med-and-down font2">Meal Planner</span></a>
-            <a class="sidenav-trigger right" data-target="mobile-links">
+            <a class="sidenav-trigger right" data-target="mobile-links" @click="toggleShowSidenav">
                 <i class="material-icons">menu</i>
             </a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="/">Home</a></li>
                 <li><a href="/favorites">Favorites</a></li>
                 <li><a href="/calendar">Calendar</a></li>
-                <!-- <li><a href="#login" id="navbar-login-top" class="modal-trigger valign-wrapper">Login
-                        <i class="material-icons right">person</i></a>
-                </li> -->
             </ul>
         </div>
     </nav>
+    <div class="side-nav" v-show="showSidenav">
+        <div class="sidenav-background" @click="toggleShowSidenav"></div>
+        <div class="light-green darken-1 grey-text" id="mobile-links">
+            <a class="close" @click="toggleShowSidenav">
+                <i class="material-icons">close</i>
+            </a>
+            <li><a href="/">Home</a></li>
+            <li><a href="/favorites">Favorites</a></li>
+            <li><a href="/calendar">Calendar</a></li>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -27,7 +35,13 @@ export default {
     },
     data() {
         return {
-            logo: '/images/white-logo.png'
+            logo: '/images/white-logo.png',
+            showSidenav: false
+        }
+    },
+    methods: {
+        toggleShowSidenav() {
+            this.showSidenav = !this.showSidenav
         }
     }
 }
@@ -42,5 +56,46 @@ export default {
 
     nav {
         height: 60px;
+    }
+
+    .side-nav {
+        z-index: 2;
+        position: fixed;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+    }
+
+    #mobile-links {
+        position: absolute;
+        height: 100%;
+        width: 150px;
+        right: 0;
+        top: 0;
+    }
+
+    #mobile-links a{
+        color: white;
+        display: block;
+        width: 100;
+        text-align: right;
+        padding: 10px 20px;
+        /* border-bottom: 1px solid white; */
+    }
+
+    #mobile-links .close{
+        margin-top: 10px;
+        cursor: pointer;
+    }    
+    .sidenav-background {
+        background-color: rgb(51, 105, 30, 0.5);
+        -webkit-backdrop-filter: blur(10px);
+        backdrop-filter: blur(10px);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
     }
 </style>
